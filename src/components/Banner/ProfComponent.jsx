96bce9408cc930e5fdb6c38d9530c1054c7834b7
Qaasim Umar <omayr010@gmail.com>
2023-03-31 22:31:24 +0100

    feat: finished with the ui

---
+++ src/components/Banner/ProfComponent.jsx
@@ -0,0 +1,179 @@
+import React from "react";
+import styled from "styled-components";
+import '../../styles/global.css'
+import { AiOutlineTwitter } from "react-icons/ai";
+import { GiEarthAmerica } from "react-icons/gi";
+import { FaLinkedinIn } from "react-icons/fa";
+import { Slide } from "react-awesome-reveal";
+// import profileImage from "../images/portfolioImg.jpg"
+import profileImage2 from "../images/profileimagewhite.png"
+import profileImage3 from "../images/profileimagenobackground.png"
+
+
+const ProfComponent = () => {
+  return (
+    <Container id="home">
+      <Slide direction="left">
+        <Texts>
+          <h4>
+            Hello <span className="green">I am</span>
+          </h4>
+          <h1 className="green">Qaasim Omar</h1>
+          <h3>FrontEnd Developer</h3>
+          <p>
+            I'm a software engineer specializing in building ( and occasionally designing ) exceptional digital experience.
+          </p>
+          <div className="button-flex">
+            <a href="https://wa.link/20ozs4"> <button>Let's talk</button></a>
+            <a href="https://wa.link/20ozs4"> <button>View Resume</button></a>
+          </div>
+          <Social>
+            <p>Check out my</p>
+            <div className="social-icons">
+              <span>
+                <a href="https://twitter.com/i_codejs">
+                  <AiOutlineTwitter />
+                </a>
+              </span>
+              <span>
+                <a href="/">
+                  <GiEarthAmerica />
+                </a>
+              </span>
+              <span>
+                <a href="https://ng.linkedin.com/in/qaasim-umar-103949195">
+                  <FaLinkedinIn />
+                </a>
+              </span>
+            </div>
+          </Social>
+        </Texts>
+      </Slide>
+      <Slide direction="right">
+        <Profile>
+          <img
+            className="profileimage"
+            src={profileImage3}
+            alt="profile"
+          />
+        </Profile>
+      </Slide>
+    </Container>
+  );
+};
+
+export default ProfComponent;
+
+const Container = styled.div`
+  display: flex;
+  gap: 2rem;
+  padding-top: 3rem;
+  justify-content:center;
+  align-items:center;
+  width: 80%;
+  max-width: 1280px;
+  margin: 0 auto;
+  z-index: 1;
+  @media (max-width: 840px) {
+    width: 90%;
+  }
+
+  @media (max-width: 640px) {
+    flex-direction: column;
+  }
+`;
+const Texts = styled.div`
+  flex: 1;
+  h4 {
+    padding: 1rem 0;
+    font-weight: 500;
+  }
+  h1 {
+    font-size: 2rem;
+    font-family: "Secular One", sans-serif;
+    letter-spacing: 2px;
+  }
+  h3 {
+    font-weight: 500;
+    font-size: 1.2rem;
+    padding-bottom: 1.2rem;
+    text-transform: capitalize;
+  }
+  p {
+    font-weight: 300;
+  }
+
+  button {
+    padding: 0.7rem 2rem;
+    margin-top: 3rem;
+    cursor: pointer;
+    background-color: #01be96;
+    border: none;
+    color: #fff;
+    font-weight: 500;
+    filter: drop-shadow(0px 10px 10px #01be9551);
+    :hover {
+      filter: drop-shadow(0px 10px 10px #01be9570);
+    }
+  }
+`;
+const Social = styled.div`
+  margin-top: 3rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  p {
+    font-size: 0.9rem;
+    @media (max-width: 690px) {
+      font-size: 0.7rem;
+    }
+  }
+
+  .social-icons {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    span {
+      width: 2.3rem;
+      height: 2rem;
+      clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+      background-color: #01be96;
+      position: relative;
+      transition: transform 400ms ease-in-out;
+      :hover {
+        transform: rotate(360deg);
+      }
+    }
+
+    a {
+      color: #fff;
+      position: absolute;
+      top: 55%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+`;
+const Profile = styled.div`
+  img {
+    width: 25rem;
+    filter: drop-shadow(0px 10px 10px #01be9570);
+    transition: transform 400ms ease-in-out;
+    background-color:#01be96;
+    @media (max-width: 790px) {
+      width: 20rem;
+    }
+
+    @media (max-width: 660px) {
+      width: 18rem;
+    }
+
+    @media (max-width: 640px) {
+      width: 100%;
+    }
+  }
+
+  :hover img {
+    transform: translateY(-10px);
+  }
+`;
